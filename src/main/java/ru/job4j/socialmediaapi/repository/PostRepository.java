3817f84b63f48955650680aa60aa56dfa59c9e6b
Slava Osipov <skybright@mail.ru>
2024-08-03 22:34:14 +0300

    task_505063 add named queries, add tests for them

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -1,7 +1,20 @@
 package ru.job4j.socialmediaapi.repository;
 
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.socialmediaapi.model.Post;
+import ru.job4j.socialmediaapi.model.User;
 
-public interface PostRepository extends ListCrudRepository<Post, Long> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> getPostsByFromUser(User user);
+
+    List<Post> getPostsByCreatedBetween(LocalDateTime from, LocalDateTime to);
+
+    Page<Post> getPostsByOrderByCreated(Pageable pageable);
+
 }
--- src/test/java/ru/job4j/socialmediaapi/repository/PostRepositoryTest.java
+++ src/test/java/ru/job4j/socialmediaapi/repository/PostRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import ru.job4j.socialmediaapi.model.File;
 import ru.job4j.socialmediaapi.model.Post;
 import ru.job4j.socialmediaapi.model.User;
@@ -13,6 +14,8 @@ import ru.job4j.socialmediaapi.model.User;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -55,6 +58,22 @@ public class PostRepositoryTest {
         user2.setPassword("password");
         userRepository.save(user2);
         return List.of(user1, user2);
+    }
+
+    private List<Post> initPostsWithDifferentDates(User userFrom, User userTo) {
+        List<Post> posts = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            var post = new Post();
+            post.setFromUser(userFrom);
+            post.setToUser(userTo);
+            post.setTitle("title %d%n".formatted(i));
+            post.setCreated(LocalDateTime.now(ZoneId.of("UTC"))
+                    .truncatedTo(ChronoUnit.SECONDS)
+                    .minusDays(i));
+            post.setIsActive(true);
+            posts.add(post);
+        }
+        return posts;
     }
 
     @Test
@@ -116,6 +135,75 @@ public class PostRepositoryTest {
         var posts = postRepository.findAll();
         assertThat(posts).hasSize(2);
         assertThat(posts).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    public void whenSavePostsThenFindByAuthor() {
+        var users = initUsers();
+        var userFrom = users.get(0);
+        var userTo = users.get(1);
+
+        var post = new Post();
+        post.setFromUser(userFrom);
+        post.setToUser(userTo);
+        post.setTitle("Hi. Its my first message");
+        post.setDescription("So. Would you like to go smwh?");
+        post.setCreated(LocalDateTime.now(ZoneId.of("UTC"))
+                .truncatedTo(ChronoUnit.SECONDS));
+        post.setIsActive(true);
+        postRepository.save(post);
+
+        var post2 = new Post();
+        post2.setFromUser(userFrom);
+        post2.setToUser(userTo);
+        post2.setTitle("Where are you?");
+        post2.setCreated(LocalDateTime.now(ZoneId.of("UTC"))
+                .truncatedTo(ChronoUnit.SECONDS));
+        post2.setIsActive(true);
+        postRepository.save(post2);
+
+        var expected = List.of(post, post2);
+        var posts = postRepository.getPostsByFromUser(userFrom);
+        assertThat(posts).hasSize(2);
+        assertThat(posts).usingRecursiveComparison().isEqualTo(expected);
+
+        posts = postRepository.getPostsByFromUser(userTo);
+        assertThat(posts).hasSize(0);
+    }
+
+    @Test
+    public void whenSavePostsThenFindBetweenDates() {
+        var users = initUsers();
+        List<Post> posts = initPostsWithDifferentDates(users.get(0), users.get(1));
+        postRepository.saveAll(posts);
+
+        var after = LocalDateTime.now(ZoneId.of("UTC"))
+                .minusDays(4);
+        var before = LocalDateTime.now(ZoneId.of("UTC"))
+                .minusHours(1);
+
+        var expected = posts.subList(1, 4);
+        var actual = postRepository.getPostsByCreatedBetween(after, before);
+        assertThat(actual).hasSize(3);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    public void whenSavePostsThenFindPageOrderByDate() {
+        var users = initUsers();
+        List<Post> posts = initPostsWithDifferentDates(users.get(0), users.get(1));
+        postRepository.saveAll(posts);
+
+        posts.sort(new Comparator<Post>() {
+            public int compare(Post o1, Post o2) {
+                return o1.getCreated().compareTo(o2.getCreated());
+            }
+        });
+        var expected = posts.subList(3, 5);
+
+        var actual = postRepository.getPostsByOrderByCreated(PageRequest.of(1, 3));
+        assertThat(actual.getContent().size()).isEqualTo(2);
+        assertThat(actual.getContent()).usingRecursiveComparison().isEqualTo(expected);
     }
 
 }
